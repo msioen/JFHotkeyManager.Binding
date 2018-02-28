0e03789ab96ee456eeb3d0aaa498e321176b9e8a
Michiel Sioen <michiel.sioen@flowpilots.com>
2018-02-28 20:37:13 +0100

    Initial Commit

---
+++ HotKeyManager/HotKeyManager.h
@@ -0,0 +1,13 @@
+//
+//  HotKeyManager.h
+//  HotKeyManager
+//
+//  Created by Michiel Sioen on 28/02/2018.
+//  Copyright © 2018 Michiel Sioen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HotKeyManager : NSObject
+
+@end
---
+++ HotKeyManager/HotKeyManager.m
@@ -0,0 +1,13 @@
+//
+//  HotKeyManager.m
+//  HotKeyManager
+//
+//  Created by Michiel Sioen on 28/02/2018.
+//  Copyright © 2018 Michiel Sioen. All rights reserved.
+//
+
+#import "HotKeyManager.h"
+
+@implementation HotKeyManager
+
+@end
